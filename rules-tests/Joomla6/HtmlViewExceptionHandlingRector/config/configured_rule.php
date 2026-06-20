8b82d13cc5a6eaaae3c5f87fca3b78ec5bfeb37a
Hannes Papenberg <info@joomlager.de>
2026-06-21 00:43:36 +0200

    Add HtmlViewExceptionHandlingRector rule

---
+++ rules-tests/Joomla6/HtmlViewExceptionHandlingRector/config/configured_rule.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package     Joomla.Rector
+ * @subpackage  Joomla6
+ *
+ * @copyright   (C) 2026 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+use Joomla\Rector\Joomla6\HtmlViewExceptionHandlingRector;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(
+        HtmlViewExceptionHandlingRector::class
+    );
+
+    $rectorConfig->autoloadPaths([
+        __DIR__ . '/../../../../joomla',
+    ]);
+};
