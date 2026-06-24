8bcfba2dbfd34c840ce4a9f9a4f8dad133aa480b
Hannes Papenberg <info@joomlager.de>
2026-06-24 13:57:35 +0200

    Add SetErrorToExceptionRector rule

---
+++ rules-tests/Joomla6/SetErrorToExceptionRector/SetErrorToExceptionRectorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package     Joomla.Rector
+ * @subpackage  Joomla6
+ *
+ * @copyright   (C) 2026 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Joomla\Rector\Tests\Joomla6\SetErrorToExceptionRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+/**
+ * @since  1.0.0
+ */
+final class SetErrorToExceptionRectorTest extends AbstractRectorTestCase
+{
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+
+    /**
+     * @return \Iterator<array<int, string>>
+     */
+    public static function provideData(): \Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function testRefactor(string $fixtureFilePath): void
+    {
+        $this->doTestFile($fixtureFilePath);
+    }
+}
